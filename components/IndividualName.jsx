62ca687a1c5d184d12dc882499836e58b4413f16
Hirtop Laura <lhirtop@gmail.com>
2021-04-24 22:52:49 +0300

    updates to admin classes and clients options

---
+++ components/IndividualName.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function IndividualName(props) {
+    return (
+        <div className="user-list-items">
+            <div>
+            <span className="list-initial">
+                A
+            </span>
+            <span className="list-name">
+                {props.name}
+                </span>
+            </div>
+            <span onClick={() => props.seeDetails(props.id)}>
+            &gt;
+            </span>
+        </div>
+    )
+  }
+  
