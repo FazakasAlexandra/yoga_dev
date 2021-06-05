ac8bdcc0063672045e293fea26b2412829b5bb82
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-06-05 08:32:03 +0300

    booking card style

--- components/BookingCard.jsx
+++ components/BookingCard.jsx
@@ -1,17 +1,40 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
 
-export default function BookingCard({ booking, }) {
-    const loop = (object, text) => {
-        return object.map(({ remained_entrences, subscription_name }) => {
-            return <p className="converage-desc"> <b>{remained_entrences}</b> {text} <br/><i> {subscription_name}</i></p>
+export default function BookingCard({ booking }) {
+
+    const loop = (object, text, event = false) => {
+        console.log(object)
+        const [isHover, setIsHover] = useState(false)
+
+        return object.map(({ remained_entrences, subscription_name, user_subscription_id }, idx) => {
+            return <div
+                style={{display: "flex"}}
+                key={user_subscription_id}
+                onMouseLeave={text === 'discount' ? null : () => setIsHover(false)}
+                onMouseEnter={text === 'discount' ? null : () => setIsHover(true)}
+                onClick={text === 'discount' ? null : () => {
+                    const yes = confirm(`Are you sure you want to decrease the amount of ${text}?`)
+                    if(yes){
+                        console.log('decrease me ', object[idx])
+                    }
+                }}>
+                {isHover ? <span><FontAwesomeIcon style={{color:'#5E54AC'}}icon={faMinus} size="l" /></span> : null}
+                <p
+                    style={{cursor : text === 'discount' ? '' : 'pointer'}}
+                    className="converage-desc"> <b>{remained_entrences}{text === 'discount' ? '%' : ''}</b> {text} <br /><i> {subscription_name}</i>
+                </p>
+            </div>
         })
     }
 
     const getClassCoverage = () => {
         return booking.user_subscriptions.map(({ free_entrences, entrences, discount }) => {
             if (free_entrences) return loop(free_entrences, 'free entrences')
-            if (discount) return loop(discount, 'free entrences')
-            if (entrences) return loop(entrences, 'free entrences')
+            if (discount) return loop(discount, 'discount')
+            if (entrences) return loop(entrences, 'entrences')
 
             return null
         })
