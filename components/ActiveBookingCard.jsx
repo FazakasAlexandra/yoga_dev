2aa157f8b9a7be37b0ad5a12a6e120c0a5690185
Hirtop Laura <lhirtop@gmail.com>
2021-06-11 17:43:53 +0300

    fix problem with data received from db

--- components/ActiveBookingCard.jsx
+++ components/ActiveBookingCard.jsx
@@ -47,7 +47,7 @@ export default function ActiveBookingCard({
         </div>
       </div>
       <ClassCoverage
-        userSubscriptions={history.user_subscriptions}
+        userSubscriptions={history.userSubscriptions}
         changeStatus={changeStatus}
       />
     </div>
