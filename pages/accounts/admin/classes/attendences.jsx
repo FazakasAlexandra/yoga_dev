40e0ad56b6f5ecc0c5530bdb2f013a4442bac39a
Hirtop Laura <lhirtop@gmail.com>
2021-07-04 15:00:02 +0300

    +empty statistics

--- pages/accounts/admin/classes/attendences.jsx
+++ pages/accounts/admin/classes/attendences.jsx
@@ -7,6 +7,7 @@ import Loader from '../../../../components/Loader'
 import AdminClassesLayout from '../../../../components/AdminClassesLayout'
 import Chart from 'react-google-charts'
 import db from '../../../../db'
+import Feedback from '../../../../components/Feedback'
 
 export default function Page() {
   const router = useRouter()
@@ -67,31 +68,38 @@ export default function Page() {
       <AdminLayout activeTab={'classes'}>
         <AdminClassesLayout activeTab={'attendences'}>
           <div className='chart-container' style={{ width: '100%' }}>
-            <Chart
-              width={'99%'}
-              height={'700px'}
-              chartType='BarChart'
-              loader={<Loader/>}
-              data={[
-                ['Class', 'This Month', 'Last Three Months', 'This Year'],
-                ...attendences,
-              ]}
-              options={{
-                title: 'Classes Attendences',
-                bar: { groupWidth: '80%' },
-                colors: ['#8BDD7C', '#83BCFF', '#F46565'],
-                fontName: 'roboto, sans-serif',
-                fontSize: 17,
-                legend: {
-                  textStyle: {
-                    fontSize: 14,
+            {attendences.length != 0 ? (
+              <Chart
+                width={'99%'}
+                height={'700px'}
+                chartType='BarChart'
+                loader={<Loader />}
+                data={[
+                  ['Class', 'This Month', 'Last Three Months', 'This Year'],
+                  ...attendences,
+                ]}
+                options={{
+                  title: 'Classes Attendences',
+                  bar: { groupWidth: '80%' },
+                  colors: ['#8BDD7C', '#83BCFF', '#F46565'],
+                  fontName: 'roboto, sans-serif',
+                  fontSize: 17,
+                  legend: {
+                    textStyle: {
+                      fontSize: 14,
+                    },
                   },
-                },
-                titleTextStyle: {
-                  fontSize: 20,
-                },
-              }}
-            />
+                  titleTextStyle: {
+                    fontSize: 20,
+                  },
+                }}
+              />
+            ) : (
+              <Feedback
+                message='There are no statistics to display!'
+                iconName='sadface'
+              />
+            )}
           </div>
         </AdminClassesLayout>
       </AdminLayout>
