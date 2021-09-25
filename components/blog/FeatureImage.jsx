3c65af54e0c212f42f4eb64b66252f205ee3ba86
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-09-25 10:40:00 +0300

    fix feature image

--- components/blog/FeatureImage.jsx
+++ components/blog/FeatureImage.jsx
@@ -1,26 +1,24 @@
 import SVG from 'react-inlinesvg';
 
 export const FeatureImage = ({ showForm, setImage, featureImage }) => {
-    return <div
-        style={{
-            marginTop: showForm ? "-294px" : "-10px",
-            cursor: setImage ? "cursor" : "default"
-        }}
-        className="svg-container"
-        onClick={setImage}
-    >
-        {
-            !featureImage ?
-                <div>
-                    {setImage && <h1>Upload picture</h1>}
-                    <SVG src="/assets/blog_placeholder.svg" />
-                </div> :
-                <img
-                    className="feature-image"
-                    alt="feature image"
-                    src={featureImage}
-                    onClick={setImage}
-                />
-        }
-    </div>
+    return (
+        !featureImage ?
+            <div
+                style={{
+                    marginTop: showForm ? "-294px" : "-10px",
+                    cursor: setImage ? "cursor" : "default"
+                }}
+                className="svg-container"
+                onClick={setImage}
+            >
+                {setImage && <h1>Upload picture</h1>}
+                <SVG src="/assets/blog_placeholder.svg" />
+            </div> :
+            <img
+                className="feature-image"
+                alt="feature image"
+                src={featureImage}
+                onClick={setImage}
+            />
+    )
 }
