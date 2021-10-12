cddb4fe336621ad695d5badf02a70d5a215e5875
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-10-12 19:29:20 +0300

    change oauth credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.PROD_GOOGLE_ID,
+      clientSecret: process.env.PROD_GOOGLE_SECRET,
     }),
   ],
   secret: process.env.SECRET,
