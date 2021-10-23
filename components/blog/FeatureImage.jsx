24fd15c035815666b0bd794a2938af2d882df950
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-10-23 13:58:02 +0300

    fix blog image

--- components/blog/FeatureImage.jsx
+++ components/blog/FeatureImage.jsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'next-i18next';
 export const FeatureImage = ({ showForm, setImage, featureImage }) => {
     const { t } = useTranslation(); 
 
-    console.log('rerenderme',featureImage)
-
     return (
         setImage && !featureImage?
             <div
